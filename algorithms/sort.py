3ecf8a3888b5eb4d8b256cd0d5fd7453f7373c8b
Saeed Andalib <saeedandalib5@gmail.com>
2020-10-20 16:07:06 +0330

    Improved Modules & Packaging

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -1,6 +1,3 @@
-from random import randint
-
-
 # Insertion Sorting Algorithm
 def insertion_sort(arr):
     for i in range(1, len(arr)):
@@ -12,11 +9,3 @@ def insertion_sort(arr):
         arr[j + 1] = key
 
     return arr
-
-
-# Test Driver
-random_list = [randint(0, 100) for x in range(15)]
-print(f'Random List: {random_list}')
-
-sorted_list = insertion_sort(random_list)
-print(f'Sorted List: {sorted_list}')
