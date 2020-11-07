d3509e22e6bb0df5540d0189b7ae11c65ee1c1bd
Saeed Andalib <saeedandalib5@gmail.com>
2020-11-07 07:56:30 +0330

    Fixed Stack

--- structures/stack.py
+++ structures/stack.py
@@ -3,13 +3,13 @@ class Stack:
         self.data = []
 
     def push(self, data):
-        self.data.insert(0, data)
+        self.data.append(data)
 
     def pop(self):
-        return self.data.pop(0)
+        return self.data.pop()
 
     def peek(self):
-        return self.data[0]
+        return self.data[-1]
 
     def is_empty(self):
         return len(self.data) == 0
